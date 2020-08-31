db3280e4c0608af197527d2bedd3dcc2bff71844
aekant <rajaaekant123@gmail.com>
2020-08-31 16:54:04 +0500

    endpoints added for a few reports generation - using mongoDB aggregation pipeline

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 // Importing the Task Model to create a task
 const Task = require('./../models/taskModel');
 const APIFeatures = require('./../utils/apiFeatures');
+const { Query } = require('mongoose');
 
 // GET routes handlers
 module.exports.getAll = async (req, res) => {
@@ -80,8 +81,21 @@ module.exports.updateById = async (req, res) => {
     // and a few config options such as running validators again
     // to ensure that user did not enter any invalid data
     let query = Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, //returns the updated document
-      runValidators: true //checks for invalid data
+      // returns the updated document
+      new: true,
+      // checks for invalid data
+      // since  we are using the update method, here the validator wont
+      // have access to the document being update using this keyword
+      // In order to run a full fledge validation check (one in which we check
+      // other field values too) we should instead use findOne() by ID and then
+      // replace the fields manually and then do .save() method. This will 
+      // trigger the validator and will have access to the document using this
+      runValidators: true,
+
+      // setting this option will enable us to access the underlying query
+      // object when running update validators
+      // doesn't work
+      // context: 'query'
     });
     const task = await query;
     res.status(200).json({
@@ -104,13 +118,88 @@ module.exports.deleteById = async (req, res) => {
     let query = Task.findByIdAndDelete(req.params.id);
     const task = await query;
     res.status(204).json({
-      message: "Success",
+      message: 'Success',
       data: null
     });
   } catch (err) {
     res.status(404).json({
       message: 'Failed',
       error: err.message
+    });
+  }
+}
+
+// Report generation routes
+module.exports.getStats = async (req, res) => {
+  try {
+    let aggr = Task.aggregate([
+      {
+        $group: {
+          // we don't want to group them by anything, we want to check all of
+          // them
+          _id: null,
+          // since we aren't grouping by anything there would be a single group
+          // all docs fall in a single group
+          // for each doc falling in this group we increment the total by 1
+          total: { $sum: 1 },
+          // $in : [<expression to check for>, [<array of values to check from>]]
+          // $cond : [<condition>, <true value>, <false value>]
+          completed: { $sum: { $cond: [{ $in: ['$status', ['COMPLETED', 'LATE-COMPLETION']] }, 1, 0] } },
+          remaining: { $sum: { $cond: [{ $in: ['$status', ['NEW', 'IN-PROGRESS', 'OVERDUE']] }, 1, 0] } }
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      }
+    ]);
+
+    let stats = await aggr;
+    res.status(200).json({
+      message: 'Success',
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: 'Failed',
+      error: err.message
+    });
+  }
+}
+
+module.exports.getStats2 = async (req, res) => {
+  try {
+    let aggr = Task.aggregate([
+      {
+        $match: { status: 'NEW' }
+      },
+      {
+        $group: {
+          _id: null,
+          total: { $sum: 1 },
+          tasks: { $push: { id: '$_id', title: '$title', deadline: '$deadline', completedAt: '$completedAt' } }
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      }
+    ]);
+    const stats = await aggr;
+    res.status(200).json({
+      message: 'Success',
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: 'Failed',
+      error: err.messages
     });
   }
 }
--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -12,11 +12,22 @@ router
   .get(taskController.getAll)
   .post(taskController.create);
 
+// If I place this route below the :id one then
+// what it will do is pass the stats as the value of id
+// so we have to make sure this route is placed before the 
+// :id one
+router
+  .route('/stats')
+  .get(taskController.getStats);
+
+router
+  .route('/stats2')
+  .get(taskController.getStats2);
+
 router
   .route('/:id')
   .get(taskController.getById)
   .patch(taskController.updateById)
   .delete(taskController.deleteById);
-
 
 module.exports = router;
