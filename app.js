31a3743cf183449e00a62eb71e59240462398623
Aekant <rajaaekant123@gmail.com>
2020-09-09 00:14:00 +0500

    sanitization, catering parameter pollution and rate limiting implemented

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const taskRouter = require('./routers/taskRouter');
 const userRouter = require('./routers/userRouter');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
+
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'You are rate limited please try again'
+});
+
+app.use(helmet());
+
+app.use('/', limiter);
 // since both of the routes that is users and tasks will require the 
 // data to be parsed onto a body object therefore we are specifying the
 // middleware here to  avoid code repetition
 app.use(express.json());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
 app.use('/tasks', taskRouter);
 app.use('/users', userRouter);
